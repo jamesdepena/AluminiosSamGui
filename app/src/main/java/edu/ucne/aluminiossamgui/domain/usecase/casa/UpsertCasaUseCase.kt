4249f053bbf0193c5092d9e17fc6ec430b614cb7
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:02:45 -0400

    Crea UpsertCasaUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/casa/UpsertCasaUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.aluminiossamgui.domain.usecase.casa
+
+import edu.ucne.aluminiossamgui.domain.model.Casa
+import edu.ucne.aluminiossamgui.domain.repository.CasaRepository
+import javax.inject.Inject
+
+class UpsertCasaUseCase @Inject constructor(
+    private val repository: CasaRepository
+) {
+    suspend operator fun invoke(casa: Casa) {
+        repository.upsert(casa)
+    }
+}
