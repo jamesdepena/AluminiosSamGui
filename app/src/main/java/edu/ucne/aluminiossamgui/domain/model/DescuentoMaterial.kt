0f0041a5419c557c4e4c8565155ddb64d8c45173
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 12:58:41 -0400

    Crea Modelo DescuentoMaterial.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/model/DescuentoMaterial.kt
@@ -0,0 +1,10 @@
+package edu.ucne.aluminiossamgui.domain.model
+
+data class DescuentoMaterial(
+    val tipoMaterial: TipoMaterial,
+    val cabezalYRielFijo: Double,
+    val lateral: Double,
+    val alfeizal: Double,
+    val llavinYEnganche: Double,
+    val tresViasAlf: Double
+)
