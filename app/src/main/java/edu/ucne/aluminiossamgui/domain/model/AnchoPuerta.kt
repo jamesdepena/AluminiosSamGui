a81857823c80b1fe096822b69715f5d7f8b51ddb
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 12:57:23 -0400

    Crea Modelo AnchoPuerta.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/model/AnchoPuerta.kt
@@ -0,0 +1,16 @@
+package edu.ucne.aluminiossamgui.domain.model
+
+enum class AnchoPuerta(
+    val centimetros: Int
+) {
+    ANCHO_60(60),
+    ANCHO_65(65),
+    ANCHO_70(70),
+    ANCHO_75(75),
+    ANCHO_80(80),
+    ANCHO_85(85),
+    ANCHO_90(90),
+    ANCHO_95(95),
+    ANCHO_100(100),
+    ANCHO_105(105)
+}
