e865d2a70e4fef0492834ff8be61fcd77dc46505
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:03:56 -0400

    Crea GetHuecoByIdUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/hueco/GetHuecoByIdUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.aluminiossamgui.domain.usecase.hueco
+
+import edu.ucne.aluminiossamgui.domain.model.Hueco
+import edu.ucne.aluminiossamgui.domain.repository.HuecoRepository
+import javax.inject.Inject
+
+class GetHuecoByIdUseCase @Inject constructor(
+    private val repository: HuecoRepository
+) {
+    suspend operator fun invoke(id: Int): Hueco? {
+        return repository.getById(id)
+    }
+}
