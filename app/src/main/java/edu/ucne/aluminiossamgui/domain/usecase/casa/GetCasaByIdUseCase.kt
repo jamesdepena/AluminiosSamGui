30de332f245cd919959995f6cb6d02f179b2795b
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:02:19 -0400

    Crea GetCasaByIdUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/casa/GetCasaByIdUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.aluminiossamgui.domain.usecase.casa
+
+import edu.ucne.aluminiossamgui.domain.model.Casa
+import edu.ucne.aluminiossamgui.domain.repository.CasaRepository
+import javax.inject.Inject
+
+class GetCasaByIdUseCase @Inject constructor(
+    private val repository: CasaRepository
+) {
+    suspend operator fun invoke(id: Int): Casa? {
+        return repository.getById(id)
+    }
+}
