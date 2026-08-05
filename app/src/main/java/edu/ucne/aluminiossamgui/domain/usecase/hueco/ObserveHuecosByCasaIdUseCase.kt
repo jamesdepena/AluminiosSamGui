f3f09aefe274c529ddb72269620a651c7413450f
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:04:18 -0400

    Crea ObserveHuecosByCasaIdUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/hueco/ObserveHuecosByCasaIdUseCase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.aluminiossamgui.domain.usecase.hueco
+
+import edu.ucne.aluminiossamgui.domain.model.Hueco
+import edu.ucne.aluminiossamgui.domain.repository.HuecoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveHuecosByCasaIdUseCase @Inject constructor(
+    private val repository: HuecoRepository
+) {
+    operator fun invoke(casaId: Int): Flow<List<Hueco>> {
+        return repository.observeByCasaId(casaId)
+    }
+}
