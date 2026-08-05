328d33d0f9ddb8a9ae4f3d0790f4e5901d474304
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 12:58:17 -0400

    Crea Modelo CorteCorredera.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/model/CorteCorredera.kt
@@ -0,0 +1,9 @@
+package edu.ucne.aluminiossamgui.domain.model
+
+data class CorteCorredera(
+    val cabezalYRielFijo: Double,
+    val lateral: Double,
+    val alfeizal: Double,
+    val llavinYEnganche: Double,
+    val tresViasAlf: Double? = null
+)
