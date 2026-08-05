14450fef41793e0d93d2169721208f38665a6ccd
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:02:33 -0400

    Crea ObserveCasasUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/casa/ObserveCasasUseCase.kt
@@ -0,0 +1,14 @@
+package edu.ucne.aluminiossamgui.domain.usecase.casa
+
+import edu.ucne.aluminiossamgui.domain.model.Casa
+import edu.ucne.aluminiossamgui.domain.repository.CasaRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveCasasUseCase @Inject constructor(
+    private val repository: CasaRepository
+) {
+    operator fun invoke(): Flow<List<Casa>> {
+        return repository.observeCasas()
+    }
+}
