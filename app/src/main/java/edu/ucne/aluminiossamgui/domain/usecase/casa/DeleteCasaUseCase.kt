a4dbaeedf81dc3700253909bcb4d535baa785e17
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 13:01:46 -0400

    Crea DeleteCasaUseCase.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/casa/DeleteCasaUseCase.kt
@@ -0,0 +1,12 @@
+package edu.ucne.aluminiossamgui.domain.usecase.casa
+
+import edu.ucne.aluminiossamgui.domain.repository.CasaRepository
+import javax.inject.Inject
+
+class DeleteCasaUseCase @Inject constructor(
+    private val repository: CasaRepository
+) {
+    suspend operator fun invoke(id: Int) {
+        repository.delete(id)
+    }
+}
