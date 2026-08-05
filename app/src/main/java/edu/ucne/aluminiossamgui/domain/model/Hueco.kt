5cada7193c85d0a1f6796402b063bd5dbfeb751b
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 12:57:08 -0400

    Crea Modelo Hueco.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/model/Hueco.kt
@@ -0,0 +1,16 @@
+package edu.ucne.aluminiossamgui.domain.model
+
+data class Hueco(
+    val huecoId: Int = 0,
+    val casaId: Int,
+    val etiqueta: String,
+    val tipo: TipoHueco,
+    val anchoBase: Double,
+    val largoBase: Double,
+    val tipoMaterial: TipoMaterial? = null,
+    val esTresVias: Boolean = false,
+    val color: ColorMaterial,
+    val colorPersonalizado: String? = null,
+    val anchoPuerta: AnchoPuerta? = null,
+    val acabadoPuerta: AcabadoPuerta? = null
+)
