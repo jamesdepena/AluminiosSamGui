c4cc15e60886fe3737a86317a1cf02c1470631d4
JP-Guillen <juan_guillen@ucne.edu.do>
2026-08-05 12:58:30 -0400

    Crea Modelo CorteCristalFijo.kt

---
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/model/CorteCristalFijo.kt
@@ -0,0 +1,8 @@
+package edu.ucne.aluminiossamgui.domain.model
+
+data class CorteCristalFijo(
+    val anchoMarco: Double,
+    val largoMarco: Double,
+    val anchoVidrio: Double,
+    val largoVidrio: Double
+)
