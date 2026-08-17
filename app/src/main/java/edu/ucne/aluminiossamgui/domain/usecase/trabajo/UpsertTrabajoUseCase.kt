346fbb3ff410d78e4d3bb4d51f5213c8f39cc862
jamesdepena <jamesdepena0@gmail.com>
2026-08-17 18:50:09 -0400

    Refactoriza para retonar Result<Unit> y agrega la validacion

--- app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/trabajo/UpsertTrabajoUseCase.kt
+++ app/src/main/java/edu/ucne/aluminiossamgui/domain/usecase/trabajo/UpsertTrabajoUseCase.kt
@@ -5,9 +5,22 @@ import edu.ucne.aluminiossamgui.domain.repository.TrabajoRepository
 import jakarta.inject.Inject
 
 class UpsertTrabajoUseCase @Inject constructor(
-    private val repository: TrabajoRepository
+    private val repository: TrabajoRepository,
+    private val validations: TrabajoValidations
 ) {
-    suspend operator fun invoke(trabajo: Trabajo) {
-        repository.upsert(trabajo)
+    suspend operator fun invoke(trabajo: Trabajo): Result<Unit> {
+        val validation = validations.validateNombre(trabajo.nombre)
+
+        if (!validation.isValid) {
+            return Result.failure(
+                IllegalArgumentException(
+                    validation.errorMsg ?: "El trabajo contiene datos inválidos."
+                )
+            )
+        }
+
+        return runCatching {
+            repository.upsert(trabajo)
+        }
     }
 }
