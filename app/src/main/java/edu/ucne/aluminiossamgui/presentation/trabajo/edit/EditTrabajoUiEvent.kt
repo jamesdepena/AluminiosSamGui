c09c8aa6d31bdc72b4f8dc2c4de7509be75b481d
jamesdepena <jamesdepena0@gmail.com>
2026-08-18 22:42:24 -0400

    Agrega nuevos eventos para los nuevos atributos

--- app/src/main/java/edu/ucne/aluminiossamgui/presentation/trabajo/edit/EditTrabajoUiEvent.kt
+++ app/src/main/java/edu/ucne/aluminiossamgui/presentation/trabajo/edit/EditTrabajoUiEvent.kt
@@ -3,7 +3,10 @@ package edu.ucne.aluminiossamgui.presentation.trabajo.edit
 sealed interface EditTrabajoUiEvent {
     data class Load(val id: Int?) : EditTrabajoUiEvent
     data class NombreChanged(val value: String) : EditTrabajoUiEvent
+    data class NombreClienteChanged(val value: String) : EditTrabajoUiEvent
+    data class TelefonoClienteChanged(val value: String) : EditTrabajoUiEvent
     data class DireccionChanged(val value: String) : EditTrabajoUiEvent
+    data class NotasChanged(val value: String) : EditTrabajoUiEvent
     data object Save : EditTrabajoUiEvent
     data object ShowDeleteDialog : EditTrabajoUiEvent
     data object DismissDeleteDialog : EditTrabajoUiEvent
